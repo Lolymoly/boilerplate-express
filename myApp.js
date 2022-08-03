f782733876077ec2c5d7dbf1430e3fc2a49ac6dd
Tee <teetat.wis@gmail.com>
2022-08-03 14:46:18 +0700

    finish json

--- myApp.js
+++ myApp.js
@@ -1,8 +1,24 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv').config()
 
+const publicRoute = __dirname + "/public"
 
+app.use('/public', express.static(publicRoute))
 
+app.get('/', (req, res) => {
+    const fileRoute = __dirname + "/views/index.html"
+    res.sendFile(fileRoute)
+})
+
+app.get('/json', (req, res) => {
+    const message = process.env.MESSAGE_STYLE
+    var response
+    if(message == "allCaps") response = "HELLO WORLD"
+    else response = "Hello World"
+
+    res.json({"message" : response})
+})
 
 
 
